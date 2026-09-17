76226946695dda4778a70e551aa28591011f7ad4
carbonsnake <116588615+carbonsnake@users.noreply.github.com>
2026-09-18 00:06:12 +0100

    add a cursor hover effect to the grid

--- script.js
+++ script.js
@@ -4,8 +4,6 @@ const container = document.querySelector(".container");
 
 for (let x = 1; x <= 16; x++) {
     const div = document.createElement("div");
-    div.classList.add("column");
-
     for (let i = 1; i <= 16; i++) {
         const square = document.createElement("div");
         // square.textContent = `${i}`;
@@ -14,3 +12,17 @@ for (let x = 1; x <= 16; x++) {
     }
     container.appendChild(div);
 }
+
+const squares = document.querySelectorAll(".square");
+
+squares.forEach((square) => {
+    square.addEventListener("mouseenter", (e) => {
+        e.target.classList.add("turn-purple");
+    })
+
+    square.addEventListener("mouseleave", (e) => {
+        setTimeout(() => {
+            e.target.classList.remove("turn-purple");
+        }, 50);
+    })
+})
