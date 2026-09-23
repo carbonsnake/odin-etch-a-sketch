d7a82040b35df80a93f75019374f4044e6c39501
carbonsnake <116588615+carbonsnake@users.noreply.github.com>
2026-09-23 16:10:48 +0100

    refactor code

--- script.js
+++ script.js
@@ -1,70 +1,72 @@
 "use strict";
 
-const btn = document.querySelector("#prompt-btn");
 const container = document.querySelector(".container");
-const containerCSS = document.styleSheets[0].cssRules[3].style;
-const containerWidth = containerCSS.getPropertyValue("width");
-const CONTAINER_WIDTH = containerWidth.replace("px", "");
+const btn = document.querySelector("#prompt-btn");
 
-function createGrid(side) {
-    for (let x = 1; x <= side; x++) {
-        const column = document.createElement("div");
-        column.classList.add("column");
-        
-            for (let i = 1; i <= side; i++) {
-                const square = document.createElement("div");
-                square.classList.add("square");
-                column.appendChild(square);
-                square.style.setProperty("width", `${CONTAINER_WIDTH / side}px`);
-            }
+function createGrid(sides) {
+	const containerRule = [...document.styleSheets[0].cssRules].find(
+		(r) => r.selectorText === ".container",
+	).style;
+	const CONTAINER_WIDTH = containerRule
+		.getPropertyValue("width")
+		.replace("px", "");
 
-        container.appendChild(column);
-    }
+	for (let x = 1; x <= sides; x++) {
+		const column = document.createElement("div");
 
-    const squares = document.querySelectorAll(".square");
-
-    squares.forEach((square) => {
-        square.addEventListener("mouseenter", (e) => {
-            if (e.target.style.getPropertyValue("background-color")) {
-                const currentOpacity = e.target.style.getPropertyValue("opacity");
-                e.target.style.setProperty("opacity", (Number(currentOpacity) + 0.1));
-            } else {
-                e.target.style.setProperty("background-color", randomRGBValues());
-                e.target.style.setProperty("opacity", 0.1);
-            }
-        })
-
-        // square.addEventListener("mouseleave", (e) => {
-        //     setTimeout(() => {
-        //         e.target.classList.remove("turn-purple");
-        //     }, 50);
-        // })
-    })
-}
-
-createGrid(16);
-
-function resetGrid() {
-    while (container.firstChild) {
-        container.removeChild(container.firstChild);
-    }
-}
-
-btn.addEventListener("click", () => {
-    let getDimension = prompt("please enter the number of squares per side.", "e.g. enter '5' for a 5x5 grid");
-
-    if (getDimension > 100 || getDimension < 1) {
-        alert("please enter a number between 1 and 100");
-    } else {
-        resetGrid();
-        createGrid(getDimension);
-    }
-});
-
-function randomRGBValues() {
-    return `rgb(${randomNum()}, ${randomNum()}, ${randomNum()})`
+		for (let i = 1; i <= sides; i++) {
+			const square = document.createElement("div");
+			square.classList.add("square");
+			square.style.setProperty("width", `${CONTAINER_WIDTH / sides}px`);
+			column.appendChild(square);
+		}
+		container.appendChild(column);
+	}
+	cursorHover();
 }
 
 function randomNum() {
-    return Math.floor(Math.random() * 256)
+	return Math.floor(Math.random() * 256);
 }
+
+function randomRGB() {
+	return `rgb(${randomNum()}, ${randomNum()}, ${randomNum()})`;
+}
+
+function cursorHover() {
+	const squares = document.querySelectorAll(".square");
+	squares.forEach((square) => {
+		square.addEventListener("mouseenter", (e) => {
+			const targetStyle = e.target.style;
+			if (targetStyle.getPropertyValue("background-color")) {
+				const currentOpacity = Number(targetStyle.getPropertyValue("opacity"));
+				targetStyle.setProperty("opacity", currentOpacity + 0.1);
+			} else {
+				targetStyle.setProperty("background-color", randomRGB());
+				targetStyle.setProperty("opacity", 0.1);
+			}
+		});
+	});
+}
+
+function resetGrid() {
+	while (container.firstChild) {
+		container.removeChild(container.firstChild);
+	}
+}
+
+btn.addEventListener("click", () => {
+	let getSides = prompt(
+		"please enter the number of squares per side.",
+		"e.g. enter '5' for a 5x5 grid",
+	);
+
+	if (getSides > 100 || getSides < 1) {
+		alert("please enter a number between 1 and 100");
+	} else {
+		resetGrid();
+		createGrid(getSides);
+	}
+});
+
+createGrid(16);
